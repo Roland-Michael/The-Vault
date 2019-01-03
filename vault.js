22375d06f173a15e0141c6d16152ef63383d5648
roland <roland.manangan@gamil.com>
2019-01-02 18:42:09 -1000

    done

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+	const password = {};
+
+	function setValue(key, value) {
+		password[key] = value;
+	};
+
+	function getValue(key) {
+		if (!key || !password[key]) {
+			return null
+		} else {
+
+			return password[key];
+		}
+	};
+	return {
+		setValue,
+		getValue
+	};
 };
